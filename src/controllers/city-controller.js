170410a5b91a418dfab3f55aaeb02bfbbed9bfb4
Tushar Bansal <tusharbansal069@gmail.com>
2024-03-10 17:16:37 +0530

    added City get, delete and update api's

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -16,6 +16,56 @@ async function createCity(req, res) {
   }
 }
 
+async function getCities(req, res) {
+  try {
+    const cities = await CityService.getCities();
+    SuccessResponse.data = cities;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+async function getCity(req, res) {
+  try {
+    const city = await CityService.getCity(req.params.id);
+    SuccessResponse.data = city;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+async function deleteCity(req, res) {
+  try {
+    const response = await CityService.deleteCity(req.params.id);
+    SuccessResponse.data = response;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+async function updateCity(req, res) {
+  try {
+    const response = await CityService.updateCity(req.params.id, {
+      name: req.body.name,
+    });
+    SuccessResponse.data = response;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
 module.exports = {
   createCity,
+  getCity,
+  deleteCity,
+  getCities,
+  updateCity,
 };
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -27,6 +27,76 @@ async function createCity(data) {
   }
 }
 
+async function getCity(id) {
+  try {
+    const city = await cityRepository.get(id);
+    return city;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The city you requested is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot fetch data of the city with id: " + id,
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function getCities() {
+  try {
+    const cities = await cityRepository.getAll();
+    return cities;
+  } catch (error) {
+    throw new AppError(
+      "Cannot fetch data of all cities",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function deleteCity(id) {
+  try {
+    const response = await cityRepository.destroy(id);
+    return response;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The city you requested to delete is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot fetch data of the city with id: " + id,
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function updateCity(id, data) {
+  try {
+    const response = await cityRepository.update(id, data);
+    return response;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The city you requested to update is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot update the name of the city with id: " + id,
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
   createCity,
+  deleteCity,
+  getCity,
+  getCities,
+  updateCity,
 };
