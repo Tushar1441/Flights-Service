fdba09a18a97023b6121357a2d486dd683a1da98
Tushar Bansal <tusharbansal069@gmail.com>
2024-03-10 15:53:07 +0530

    Added get airplane API

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -29,9 +29,26 @@ async function getAirplanes() {
     const airplanes = await airplaneRepository.getAll();
     return airplanes;
   } catch (error) {
-    console.log(error);
     throw new AppError(
       "Cannot fetch data of all airplanes",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function getAirplane(id) {
+  try {
+    const airplane = await airplaneRepository.get(id);
+    return airplane;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The airplane you requested is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot fetch data of the airplane with id: " + id,
       StatusCodes.INTERNAL_SERVER_ERROR
     );
   }
@@ -40,4 +57,5 @@ async function getAirplanes() {
 module.exports = {
   createAirplane,
   getAirplanes,
+  getAirplane,
 };
